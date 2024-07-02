f87e2987083b56d257ce116392ab5ed48b79d832
Anav Chand <anavkki@gmail.com>
2024-07-02 17:59:39 +1200

    feat: add helpful keybinds

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,21 +1,10 @@
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 local nvim_lsp = require('lspconfig')
-local servers = { "tsserver", "gopls", "jsonls", "cssls", "prismals", "tailwindcss" }
+local servers = { "tsserver", "gopls", "jsonls", "cssls", "html", "astro", "prismals", "tailwindcss" }
 
 for _, lsp in ipairs(servers) do
     nvim_lsp[lsp].setup {
         capabilities = capabilities
     }
 end
-
-require('lspconfig').clangd.setup {
-    filetypes = { "c", "cpp" },
-    capabilities = capabilities,
-    cmd = { 'clangd', '--background-index' },
-    include = {
-        'C:/Program Files/MinGW/include',
-        'C:/Program Files/LLVM/bin/clangd.exe',
-        'C:/Program Files/MinGW/lib/gcc/mingw32/6.3.0/include/c++'
-    },
-}
--- lua/godzilla/remap.lua
+++ lua/godzilla/remap.lua
@@ -1,26 +1,37 @@
 vim.g.mapleader = " "
 
-vim.keymap.set('n', '<leader>t', ':TodoTelescope<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>g', ':LazyGit<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>q', ':bd<CR>', { noremap = true, silent = true })
-vim.keymap.set("n", "<S-u>", "<C-r>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>d", "gd", { noremap = true, silent = true })
+local keymap = vim.keymap
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+keymap.set('n', '<leader>tt', ':TodoTelescope<CR>', { noremap = true, silent = true })
+keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+keymap.set('n', '<leader>g', ':LazyGit<CR>', { noremap = true, silent = true })
+keymap.set('n', '<leader>q', ':bd<CR>', { noremap = true, silent = true })
+keymap.set("n", "<leader>a", "ggVG", { noremap = true, silent = true })
+keymap.set("n", "<S-u>", "<C-r>", { noremap = true, silent = true })
+keymap.set("n", "<leader>d", "gd", { noremap = true, silent = true })
+keymap.set("i", "<C-h>", "<C-W>", { noremap = true, silent = true })
 
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "n", "nzzzv")
+keymap.set('n', '<leader>t1', '<cmd>1ToggleTerm<CR>', {noremap = true, silent = true})
+keymap.set('n', '<leader>t2', '<cmd>2ToggleTerm<CR>', {noremap = true, silent = true})
+keymap.set('n', '<leader>t3', '<cmd>3ToggleTerm<CR>', {noremap = true, silent = true})
+keymap.set('n', '<leader>t4', '<cmd>3ToggleTerm<CR>', {noremap = true, silent = true})
+keymap.set('n', '<leader>t5', '<cmd>3ToggleTerm<CR>', {noremap = true, silent = true})
+keymap.set('n', '<leader>t6', '<cmd>3ToggleTerm<CR>', {noremap = true, silent = true})
 
-vim.keymap.set("x", "<leader>p", [["_dP]])
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+keymap.set("n", "n", "nzzzv")
+keymap.set("n", "n", "nzzzv")
 
-vim.keymap.set("n", "<S-h>", ":BufferLineCyclePrev<CR>", { noremap = true, silent = true }) 
-vim.keymap.set("n", "<S-l>", ":BufferLineCycleNext<CR>", { noremap = true, silent = true })
+keymap.set("x", "<leader>p", [["_dP]])
+keymap.set({"n", "v"}, "<leader>y", [["+y]])
 
-vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) 
-vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
-vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
+keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+keymap.set("n", "<S-h>", ":BufferLineCyclePrev<CR>", { noremap = true, silent = true }) 
+keymap.set("n", "<S-l>", ":BufferLineCycleNext<CR>", { noremap = true, silent = true })
+
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) 
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
